54bc75b8ef3ceea750dabc2830b5051ebc7975d8
Brian Nagata <btnagata@gmail.com>
2018-04-06 12:49:08 -0700

    added ability to query by individual users

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import os
 import base64
-
 from flask import Flask, render_template, request, redirect, url_for, session
+from model import Donation, Donor
 
-from model import Donation 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/donations/<user>/')
+def specific(user):
+    donations = Donation.select().join(Donor).where(Donor.name == user)
+    return render_template('donations.jinja2', donations=donations)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
